fd6dcc524df10960621d0842d84fd4c873bcef0d
mu7kata <mu7kata@icloud.com>
2020-09-14 00:44:21 +0900

    index.php フロント部分作成 #2

--- StudyDetail.php
+++ StudyDetail.php
@@ -6,43 +6,144 @@
       <h2><span>【○/○の学習履歴】</span>本日は<span>○</span>時間学習しています</h2>
     </div>
     <table>
-    <thead>
-    <tr>
-    <th class="size_s">日付</th>
-    <th class='size_m'>カテゴリ</th>
-    <th class="size_s">時間(分)</th>
-    <th class="size_l">内容</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-    <td class="size_s">○/○</td>
-    <td class='size_m'>テストテストテスト</td>
-    <td class="size_s">テスト</td>
-    <td class="size_l">テストテストテストテストテストテストテストテストテスト</td>
-    </tr>
-    </tbody>
+      <thead>
+        <tr>
+          <th class="size_s">日付</th>
+          <th class='size_m'>カテゴリ</th>
+          <th class="size_s">時間(分)</th>
+          <th class="size_l">内容</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="size_s">○/○</td>
+          <td class='size_m'>テストテストテスト</td>
+          <td class="size_s">テスト</td>
+          <td class="size_l">テストテストテストテストテストテストテストテストテスト</td>
+        </tr>
+      </tbody>
     </table>
+  </section>
+  <section class="data-list">
+    <div class="data">
+      <ul class="data-ul">
+        <li class="data_month">
+          <div>
+            <h1 class="data-title">○月○日のデータ</h1>
+              <p class="data-scope"><span>○/○</span>〜<span>○/○</span></p>
+              <p class="data-title2">学習時間</p>
+              <p class="study-time"><span>○</span>h</p>
+          </div>
+        </li>
+        <li class="data_month">
+          <div>
+            <h1 class="data-title">○月○日のデータ</h1>
+              <p class="data-scope"><span>○/○</span>〜<span>○/○</span></p>
+              <p class="data-title2">学習時間</p>
+              <p class="study-time"><span>○</span>h</p>
+          </div>
+        </li>
+        <li class="data_month">
+        <div >
+            <h1 class="data-title" >○月○日のデータ</h1>
+            <p class="data-scope"><span>○/○</span>〜<span>○/○</span></p>
+            <p class="data-title2">学習時間</p>
+            <p class="study-time"><span>○</span>h</p>
+            </div>
+        </li>
+      </ul>
+
+    </div>
+    <div class="data_space">
+    <h1>学習開始日：<span>○</span>h</h1>
+    <h1>開始から　：<span>○</span>日</h1>
+    <h1>総学習時間：<span>○</span>h</h1>
+    <h1>平均　　　：<span>○</span>h</h1>
+    </div>
   </section>
 </div>
 <style>
-.table-title{
-  text-align: center;
-  padding-bottom: 20px;
-  font-size: 25px;
-}
-td{
-  background-color: #fff;
-  height: 40px;
-}
-.size_s{
-  width: 80px;
-  text-align: center;
-}
-.size_m{
-  width: 170px;
-}
-.size_l{
-  width: 700px;
-}
+  .table-title {
+    text-align: center;
+    padding-bottom: 20px;
+    font-size: 25px;
+  }
+
+  td {
+    background-color: #fff;
+    height: 40px;
+  }
+
+  .size_s {
+    width: 80px;
+    text-align: center;
+  }
+
+  .size_m {
+    width: 170px;
+  }
+
+  .size_l {
+    width: 700px;
+  }
+
+  .data {
+    width: 600px;
+    margin: 10px;
+    float: left;
+  }
+
+  .data_space {
+    height: 300px;
+    width: 300px;
+    margin: 10px;
+    background-color: #fff;
+    float: right;
+    margin-top: 56px;
+    
+  }
+  .data_space h1{
+    margin-left: 35px;
+    margin-top: 22px;
+    font-size: 30px;
+  }
+  .data_month {
+    height: 300px;
+    width: 300px;
+    margin-top: 55px;
+    background-color: #fff;
+    display: inline-block;
+    margin-top: 30px;
+    margin-right: 30px;
+  }
+
+
+
+  .data-ul {
+    overflow-x: auto;
+    white-space: nowrap;
+    -webkit-overflow-scrolling: touch;
+    width: 600px;
+    padding: 0;
+    text-align: center;
+  }
+
+  .data-title {
+    margin-top: 22px;
+  }
+
+  .data-scope {
+    font-size:24px;
+    margin: 0;
+  }
+
+  .data-title2 {
+    font-size:30px;
+    margin: 0;
+  }
+
+  .study-time {
+    font-size: 60px;
+    margin: 0;
+  }
 </style>
--- index.php
+++ index.php
@@ -8,27 +8,75 @@
 <style>
 body{
     margin: 0;
-    background-color: #ddd;
+    background-color: #fff;
+
 }
 .site-width {
     margin: 0 auto;
     width: 980px;
-    padding-bottom: 70px;
+    padding-bottom: 170px;
+    padding-top: 60px;
 }
 
+.contents_butan{
+    float: right;
+    width: 436px;
+
+}
+.contents_butan p{
+    margin: 0;
+}
+.contents_butan a{
+    display: block;
+    height: 81px;
+    padding-top: 32px;
+    font-size: 32px;
+    text-decoration: none;
+    text-align: center;
+    color: #333;
+}
+.contents1{
+border: 1px solid #333;
+width: 300px;
+margin-bottom: 42px;
+margin-top: 15px;
+background-color: #F0FFFF;
+}
+
+
+.contents2{
+border: 1px solid #333;
+width: 300px;
+background-color: #F0FFFF;
+}
+
+
+.memo_space{
+height: 200px;
+width: 350px;
+margin-left: 83px;
+}
+.memo_space h1{
+margin: 0;
+}
+main{
+background-color: #ddd;
+height: 1200px;
+}
 </style>
+<body>
 <?php
 require('header.php');
 ?>
-<body>
+<main>
 <div class="site-width">
 <section class="contents_butan">
-<div class="contents">
+<div class="contents1">
           <a href="FillOut.php">
             <p>学習を記録する</p>
           </a>
         </div>
-        <div class="contents">
+        <div class="contents2">
           <a href="ReadBack.php">
             <p>学習を振り返る</p>
           </a>
@@ -37,12 +85,16 @@ require('header.php');
 <section　class='memo'>
 <div class="memo_space">
 <h1>メモ</h1>
+<textarea name="memo" id="" cols="50" rows="12"></textarea>
+
 </div>
 </section>
 </div>
 <?php
 require('StudyDetail.php');
 ?>
+
+</main>
 </body>
 </html>
     
