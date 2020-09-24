feda5f19ed285f4dd16a0748e574a24b0e3661a6
mu7kata <mu7kata@icloud.com>
2020-09-24 22:28:11 +0900

    fillout機能に学習時間表示機能を追加

--- StudyDetail_use.php
+++ StudyDetail_use.php
@@ -1,46 +1,123 @@
-<div class="site-width">
-  <div class="page-title">
-  </div>
-  <section>
-    <div class="table-title">
-      <h2><span>【○/○の学習履歴】</span>
+<?php
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debug('「学習振り返りページ　');
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+
+$getagtstudy = getagtstudy($u_id);
+$user = getuser($u_id);
+debug('user:' . print_r($user, true));
+$includecategory = '';
+$startdate = date('Y-m-d', strtotime($user['create_date']));
+$getstudytime = getstudytime($u_id, $startdate, date('Y-m-d'), $includecategory);
+$sutdy_period = ((strtotime(date('Y-m-d')) - strtotime($startdate)) / 86400);
+$getstudy = getstudy($u_id, date('Y-m-d'), date('Y-m-d'), $includecategory);
+?>
+<div class="study_detail">
+  <div class="site-width">
+    <div class="page-title">
     </div>
-    <table>
-      <thead>
-        <tr>
-          <th class="size_s">日付</th>
-          <th class='size_m'>カテゴリ</th>
-          <th class="size_s">時間(分)</th>
-          <th class="size_l">内容</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td class="size_s">○/○</td>
-          <td class='size_m'>テストテストテスト</td>
-          <td class="size_s">テスト</td>
-          <td class="size_l">テストテストテストテストテストテストテストテストテスト</td>
-        </tr>
-        <tr>
-          <td class="size_s">○/○</td>
-          <td class='size_m'>テストテストテスト</td>
-          <td class="size_s">テスト</td>
-          <td class="size_l">テストテストテストテストテストテストテストテストテスト</td>
-        </tr>
-      </tbody>
-    </table>
-  </section>
+    <section class="today_list">
+      <div class="table-title">
+        <h2><span>本日<span><?php echo date('m/d'); ?></span>の学習履歴　(計<span class="today_time"><?php echo round(array_shift(getstudytime($u_id, date('Y-m-d'), date('Y-m-d'), $includecategory))/60,1); ?></span>h)</span>
+
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th class="size_s">日付</th>
+            <th class='size_m'>カテゴリ</th>
+            <th class="size_s">時間(分)</th>
+            <th class="size_l">内容</th>
+          </tr>
+        </thead>
+        <tbody class="alltbody">
+          <?php foreach ($getstudy as $key => $val) { ?>
+            <tr>
+              <td class="size_s"><?php echo $val['study_date']; ?></td>
+              <td class='size_m'><?php echo $val['study_category']; ?></td>
+              <td class="size_s"><?php echo $val['study_time']; ?></td>
+              <td class="size_l"><?php echo $val['study_detail']; ?></td>
+              <td class="size_s"><a href="Edit_study.php">編集</a></td>
+            </tr>
+          <?php } ?>
+
+
+        </tbody>
+
+      </table>
+    </section>
   </div>
+</div>
 <style>
+td a{
+  font-size: 15px;
+}
+
+
+  .today_list {
+    height: 450px;
+
+  }
+
+  .table-title h1 {
+    font-size: 40px;
+
+  }
+
+  .total-study {
+    background-color: #ddd;
+    height: 553px;
+    margin: 0 auto;
+    width: 980px;
+    padding-bottom: 10px;
+    padding-top: 10px;
+
+  }
+thead{
+  display: block;
+}
+  .study_detail {
+    height: 471px;
+    background-color: #fff;
+  }
+
+  .alltbody {
+    overflow-y: auto;
+    white-space: nowrap;
+    -webkit-overflow-scrolling: touch;
+    display: block;
+    width: 1080px;
+    height: 270px;
+  }
+
+  
+
+  .startdate {
+    font-size: 21px;
+    width: 95px;
+    float: right;
+    display: block;
+    margin: 0px;
+    margin-top: -9px;
+  }
+
+  .sum {
+    font-size: 50px;
+  }
+
+  .avg {
+    font-size: 30px;
+  }
+
   .table-title {
     text-align: center;
-    padding-bottom: 20px;
     font-size: 25px;
   }
 
   td {
-    background-color: #fff;
+    color: #333;
     height: 40px;
+    background-color: #DDDDDD;
   }
 
   .size_s {
@@ -49,12 +126,16 @@
   }
 
   .size_m {
-    width: 170px;
+    width: 130px;
   }
 
   .size_l {
     width: 700px;
   }
 
+  
 
+
+ 
+ 
 </style>
