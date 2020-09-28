6eab21d1551a5eeaf3295636964a73ac114fc878
mu7kata <mu7kata@icloud.com>
2020-09-28 16:15:55 +0900

    メモ機能実装

--- index.php
+++ index.php
@@ -7,6 +7,14 @@ debugLogStart();
 $_SESSION['file'] =  basename(__FILE__);
 $u_id = $_SESSION['user_id'];
 
+
+
+if (!empty($_POST)){
+  $_SESSION['memo']=$_POST['memo'];
+ 
+  }
+  $memo = $_SESSION['memo'];
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -17,63 +25,6 @@ $u_id = $_SESSION['user_id'];
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
-<style>
-
-.contents_butan{
-    float: right;
-    width: 436px;
-
-}
-.contents_butan p{
-    margin: 0;
-}
-.contents_butan a{
-    display: block;
-    height: 81px;
-    padding-top: 32px;
-    font-size: 32px;
-    text-decoration: none;
-    text-align: center;
-    color: #333;
-}
-.contents1{
-border: 1px solid #333;
-width: 300px;
-margin-bottom: 42px;
-margin-top: 15px;
-background-color: #F0FFFF;
-box-shadow: 0 5px 15px 0 rgba(0, 0, 0, .5);
-}
-
-
-.contents2{
-border: 1px solid #333;
-width: 300px;
-background-color: #F0FFFF;
-box-shadow: 0 5px 15px 0 rgba(0, 0, 0, .5);
-}
-
-
-.memo_space{
-height: 200px;
-width: 350px;
-margin-left: 83px;
-
-}
-textarea{
-  box-shadow: 0 5px 15px 0 rgba(0, 0, 0, .5);
-}
-.memo_space h1{
-margin: 0;
-}
-.site-width2{
-  margin: 0 auto;
-  width: 980px;
-  padding-bottom: 170px;
-  padding-top: 60px;
-}
-
-</style>
 <body>
 <?php
 require('header.php');
@@ -97,7 +48,10 @@ require('auth.php');
 <section　class='memo'>
 <div class="memo_space">
 <h1>メモ</h1>
-<textarea name="memo" id="" cols="50" rows="12"></textarea>
+<form method="post">
+  <textarea name="memo" id="" cols="50" rows="12"><?php echo $memo ;?></textarea>
+  <input type="submit" name="save" id="" value="メモを保存">
+</form>
 
 </div>
 </section>
@@ -108,5 +62,64 @@ require('StudyDetail.php');
 
 </main>
 </body>
+  <style>
+  
+  .contents_butan{
+      float: right;
+      width: 436px;
+  
+  }
+  .contents_butan p{
+      margin: 0;
+  }
+  .contents_butan a{
+      display: block;
+      height: 81px;
+      padding-top: 32px;
+      font-size: 32px;
+      text-decoration: none;
+      text-align: center;
+      color: #333;
+  }
+  .contents1{
+  border: 1px solid #333;
+  width: 300px;
+  margin-bottom: 42px;
+  margin-top: 15px;
+  background-color: #F0FFFF;
+  box-shadow: 0 5px 15px 0 rgba(0, 0, 0, .5);
+  }
+  
+  
+  .contents2{
+  border: 1px solid #333;
+  width: 300px;
+  background-color: #F0FFFF;
+  box-shadow: 0 5px 15px 0 rgba(0, 0, 0, .5);
+  }
+  
+  
+  .memo_space{
+  height: 200px;
+  width: 350px;
+  margin-left: 83px;
+  
+  }
+  textarea{
+    font-size: 28px;
+    height: 208px;
+    width: 368px;
+  }
+  .memo_space h1{
+  margin: 0;
+  }
+  .site-width2{
+    margin: 0 auto;
+    width: 980px;
+    padding-bottom: 170px;
+    padding-top: 60px;
+  }
+  
+  </style>
 </html>
     
