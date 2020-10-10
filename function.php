48096473461762bd76b6efe2d5e00bda85a6b67f
mu7kata <mu7kata@icloud.com>
2020-10-10 12:58:12 +0900

    ユーザー情報hide

--- function.php
+++ function.php
@@ -64,11 +64,11 @@ function vaildEmail($str, $key)
   }
 }
 function dbConnect()
-{mysql://
+{
   
   $dsn = 'mysql:dbname=heroku_2adfdebfa87fb7d;host=us-cdbr-east-02.cleardb.com;charset=utf8';
-  $user = 'bc453247f5348c';
-  $password = 'e4d50d3d';
+  $user = 'ユーザー名';
+  $password = 'パス';
   $option = array(
 
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
